cc1113d032bb5a4c4c93c4f3141f23f0ae7f7e49
Joonatan Renel@work <joonatanrenel@gmail.com>
2018-04-02 20:30:32 +0300

    Replaced the left and right arrow with the up and down arrow for the bargraph prototype code.

--- LOGIC/Menus/Bargraph.c
+++ LOGIC/Menus/Bargraph.c
@@ -12,41 +12,42 @@
 #define BARGRAPH_WIDTH    100u
 #define BARGRAPH_HEIGHT     4u
 
-//
-//  Image data for arrow_right
-//
-const U8 rightArrowBitmaps[] =
-{
 
-    0xC0, 0xC0, 0xC0, 0xC0, 0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00, 0x00, 0x00, 0x00,
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x3E, 0x1C, 0x08,
-    0x01, 0x01, 0x01, 0x01, 0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
+const U8 monochrom_arrow_upBitmaps[] =
+{
+    0x00, 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF, 0xFE, 0xFC, 0xF8, 0xF0, 0xE0, 0xC0, 0x80, 0x00,
+    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
 };
 
-Private const Bitmap rightArrowBmp =
+// Bitmap sizes for monochrom_arrow_up
+#define monochrom_arrow_upWidthPixels 17u
+#define monochrom_arrow_upHeightPixels 9u
+
+Private const Bitmap upArrowBitmap =
 {
- .bmp_data = rightArrowBitmaps,
- .height = 23u,
- .width = 16u
+     .bmp_data = monochrom_arrow_upBitmaps,
+     .height = monochrom_arrow_upHeightPixels,
+     .width = monochrom_arrow_upWidthPixels
 };
 
-//
-//  Image data for arrow_left
-//
 
-const U8 arrow_leftBitmaps [] =
+const U8 monochrom_arrow_downBitmaps[] =
 {
-    0x00, 0x00, 0x00, 0x00, 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF, 0xC0, 0xC0, 0xC0, 0xC0,
-    0x08, 0x1C, 0x3E, 0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0x01, 0x01, 0x01, 0x01,
+    0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF, 0xFF, 0xFF, 0x7F, 0x3F, 0x1F, 0x0F, 0x07, 0x03, 0x01,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-Private const Bitmap leftArrowBmp =
+// Bitmap sizes for monochrom_arrow_down
+#define monochrom_arrow_downWidthPixels 17u
+#define monochrom_arrow_downHeightPixels 9u
+
+Private const Bitmap downArrowBitmap =
 {
- .bmp_data = arrow_leftBitmaps,
- .height = 23u,
- .width = 16u
+     .bmp_data = monochrom_arrow_downBitmaps,
+     .height = monochrom_arrow_downHeightPixels,
+     .width = monochrom_arrow_downWidthPixels
 };
+
 
 /******************/
 
@@ -69,7 +70,7 @@ Private BarGraph_T * priv_active_bar;
 /***************************** Private function forward declarations  ******************************/
 
 Private void drawBarGraph(void);
-Private void drawArrows(void);
+Private void drawBackGround(void);
 
 
 /***************************** Public function definitions  ******************************/
@@ -79,7 +80,7 @@ Private void drawArrows(void);
 Public void enterBarGraph(BarGraph_T * bar)
 {
     priv_active_bar = bar;
-    drawArrows();
+    drawBackGround();
     drawBarGraph();
 }
 
@@ -103,11 +104,14 @@ Private void drawBarGraph(void)
 
 
 /* Draws the arrows on the scrollbar sides. */
-Private void drawArrows(void)
+Private void drawBackGround(void)
 {
-    //Draw left arrow.
-    display_drawBitmapCenter(&leftArrowBmp, 10u, 32u , FALSE);
+    //Draw up arrow.
+    display_drawBitmapCenter(&upArrowBitmap, 64u, 10u , FALSE);
 
-    //Draw right arrow.
-    display_drawBitmapCenter(&rightArrowBmp, 118u, 32u, FALSE);
+    //Draw down arrow.
+    display_drawBitmapCenter(&downArrowBitmap, 64u, 54u, FALSE);
+
+    //Draw rectangle around the bar.
+    /* TODO */
 }
