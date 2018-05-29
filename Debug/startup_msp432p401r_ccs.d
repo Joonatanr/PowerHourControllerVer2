c5e6c41d5babaaf06537959292e191c60444faec
Joonatan Renel@work <joonatanrenel@gmail.com>
2018-05-29 13:59:46 +0300

    Unversioned items from the debug folder.

--- Debug/startup_msp432p401r_ccs.d
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-startup_msp432p401r_ccs.obj: ../startup_msp432p401r_ccs.c
-startup_msp432p401r_ccs.obj: C:/ti/ccs7/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
-
-../startup_msp432p401r_ccs.c: 
-C:/ti/ccs7/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
